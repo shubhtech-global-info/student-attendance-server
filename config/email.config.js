2e840cd3849bc2de2b6b95ac63f7bb439541abbc
shubhtech-global-info <shubhtech.global.info@gmail.com>
2026-01-30 14:40:48 +0530

    Update email.config.js

--- config/email.config.js
+++ config/email.config.js
@@ -2,9 +2,9 @@ const nodemailer = require('nodemailer');
 
 // Create reusable transporter object using SMTP transport
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
